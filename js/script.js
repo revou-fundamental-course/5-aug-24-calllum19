e9cd43c2f0281f07b27203ebf1df93fbc73e0b89
DawnBreaker <cmcfadyen567@gmail.com>
2024-08-14 21:11:49 +0700

    testing, will work on more later

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // Javascript File for BMI Website
+// Set essential variables for BMI calculation
+function printheight() {
+    const weight = parseFloat(document.getElementById("berat-badan").value);
+    const height = parseFloat(document.getElementById("tinggi-badan").value);
+    console.log(height.toFixed(2));
+}
